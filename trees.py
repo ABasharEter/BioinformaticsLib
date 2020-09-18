3f2ab7163f6ec21126b8a68cc1e8f74bed0c9ea6
A. Bashar Eter <a.bashareter@hotmail.com>
2020-09-19 01:26:44 +0400

    small_parsimony

--- trees.py
+++ trees.py
@@ -1,3 +1,5 @@
+import kmer
+
 import numpy as np
 
 def parse_wieghtd_graph(data):
@@ -126,4 +128,187 @@ def UPGMA(D):
         D = delete_ij(D,i,j,cid,cz,k)
         n = D.shape[0]
     return g,age
-       
+
+
+def D_star(D):
+    t = np.repeat(np.sum(D,axis=0, keepdims=True),D.shape[1], axis=0)
+    D = (D.shape[0] -2)*D - t - t.T
+    np.fill_diagonal(D,0)
+    return D
+    
+
+def delete_ij2(D,i,j,cid,k):
+    n = D.shape[0]
+    if i>j:
+        i,j=j,i
+    new_D = D
+    new_D = np.delete(new_D, (i,j), axis=0)
+    new_D = np.delete(new_D, (i,j), axis=1)
+    new_row = np.array([D[i,k]+D[j,k] - D[i,j] for k in range(D.shape[0])])
+    new_row = new_row/2
+    new_row[:n-2] = np.delete(new_row, (i,j))
+    new_row[n-2] = 0
+    new_D = np.vstack((new_D, new_row[:n-2]))
+    new_D = np.hstack((new_D, new_row[:n-1].reshape(-1,1)))
+    for x in range(n-1):
+        if x < j:
+            cid[x] = cid[x]
+        else:
+            cid[x] = cid[x+1]
+    for x in range(n-1):
+        if x < i:
+            cid[x] = cid[x]
+        else:
+            cid[x] = cid[x+1]
+    cid[n-2]=k
+    return new_D
+
+def neighbor_joining(D):
+    n = D.shape[0]
+    g = {i:{} for i in range(n)}
+    cid = {i:i for i in range(n)}
+    for _ in range(n-1):
+        Ds = D_star(D)
+        i,j = np.unravel_index((Ds + np.identity(n)*1e9).argmin(), Ds.shape)
+        d = Ds[i,j]
+        k = len(g)
+        if n > 2:
+            td = np.sum(D, axis=0)
+            delta = (td[i] - td[j])/(n-2)
+            lli = (D[i,j] + delta)/2
+            llj = (D[i,j] - delta)/2
+            g.setdefault(cid[i],{}).update({k:lli})
+            g.setdefault(k,{}).update({cid[i]:lli})
+            g.setdefault(cid[j],{}).update({k:llj})
+            g.setdefault(k,{}).update({cid[j]:llj})
+            D = delete_ij2(D,i,j,cid,k)
+            n = D.shape[0]
+        else:
+            lli = llj = D[0,1]
+            g.setdefault(cid[i],{}).update({cid[j]:D[0,1]})
+            g.setdefault(cid[j],{}).update({cid[i]:D[0,1]})
+    return g
+
+def read_bi_parsimony_tree_array(txt, rooted =False):
+    txt = txt.split("\n")
+    g = {}
+    s = set()
+    for l in txt[1:]:
+        u, v = l.strip().split("->")
+        g.setdefault(u,set()).add(v)
+        g.setdefault(v,set()).add(u)
+        if u.isalpha():
+            s.add(u)
+        if v.isalpha():
+            s.add(v)
+    n = int(txt[0])
+    q = list(s)
+    r = None
+    vis = set()
+    while q:
+        u = q.pop(0)
+        if u in vis:
+            continue
+        vis.add(u)
+        r = u
+        q.extend(g[u])
+    if not rooted:
+        u = None
+        for v in g[r]:
+            if not v.isalpha():
+                u = v
+                break
+        g[r].remove(u)
+        g[u].remove(r)
+        nr = '#'
+        g[nr]= [r,u]
+        r = nr
+    q = [r]
+    t = {}
+    vis = set()
+    idxs = {r:1}
+    while q:
+        u = q.pop(0)
+        if u in vis:
+            continue
+        vis.add(u)
+        if u.isalpha():
+            t[idxs[u]] = u
+        else:
+            print(u,g[u])
+            x,y = [i for i in g[u] if i not in vis]
+            idxs[x] = idxs[u]*2
+            idxs[y] = idxs[u]*2+1
+            t[idxs[u]] = None
+        q.extend(g[u])
+    return t
+very_larg = 1e18
+from pprint import pprint
+def small_parsimony(ta):
+    n = len([x for x in ta.values() if x][0])
+    tao = {k:False if v else True for k,v in ta.items()}
+    ta = {k:list(v) if v else [None]*n for k,v in ta.items()}
+    for i in range(n):
+        tp = {k:{} for k in ta.keys()}
+        for j in reversed(sorted(ta.keys())):
+            if tao[j]:
+                tp[j] = {k:min([v + (0 if k2 == k else 1) for k2,v in tp[j*2+1].items()]) + min([v + (0 if k2 == k else 1) for k2,v in tp[j*2].items()])
+                                 for k in kmer.dna_nucleotides}
+            else:
+                tp[j] = {k:(very_larg if k != ta[j][i] else 0) for k in kmer.dna_nucleotides}
+        #if i in (6,7,8):
+            #print(i)
+            #pprint(tp)
+        for j in sorted(ta.keys()):
+            m = min(tp[j].items(), key=lambda x: x[1])[0]
+            if int(j/2) > 0 and tp[j][m] >= tp[j][ta[int(j/2)][i]]:
+                ta[j][i] = ta[int(j/2)][i]
+            else:
+                ta[j][i] = m
+    ta = {k:''.join(v) for k,v in ta.items()}
+    return ta
+
+def print_ta(ta,rooted=False):
+    s = 0
+    txt = ""
+    for i in ta.keys():
+        if i*2 not in ta or i*2+1 not in ta:
+            continue
+        if not rooted and i == 1:
+            continue
+        d = kmer.hamming(ta[i],ta[i*2])
+        s += d
+        txt += f"{ta[i]}->{ta[i*2]}:{d}\n"
+        txt += f"{ta[i*2]}->{ta[i]}:{d}\n"
+        d = kmer.hamming(ta[i],ta[i*2+1])
+        s += d
+        txt += f"{ta[i]}->{ta[i*2+1]}:{d}\n"
+        txt += f"{ta[i*2+1]}->{ta[i]}:{d}\n"
+    if not rooted:
+        d = kmer.hamming(ta[2],ta[3])
+        s += d
+        txt += f"{ta[2]}->{ta[3]}:{d}\n"
+        txt += f"{ta[3]}->{ta[2]}:{d}\n"
+    return s,txt
+
+def ta2g(ta,rooted=False):
+    ta = [None] + ta
+    g = {}
+    s = 0
+    for i in range(1,int(len(ta)/2)):
+        g.setdefault(ta[i],{}).update({
+            ta[i*2]:kmer.hamming(ta[i],ta[i*2]),
+            ta[i*2+1]:kmer.hamming(ta[i],ta[i*2+1]),
+        })
+        if i >0 or rooted:
+            s += g[ta[i]][ta[i*2]] + g[ta[i]][ta[i*2+1]]
+        g.setdefault(ta[i*2], {}).update({ta[i]:g[ta[i]][ta[i*2]]})
+        g.setdefault(ta[i*2+1], {}).update({ta[i]:g[ta[i]][ta[i*2+1]]})
+    if not rooted:
+        del g[ta[1]]
+        del g[ta[2]][ta[1]]
+        del g[ta[3]][ta[1]]
+        g[ta[2]][ta[3]] = g[ta[3]][ta[2]] = kmer.hamming(v,u)
+        s += g[ta[2]][ta[3]]
+    return s,g
+                
